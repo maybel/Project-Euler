363c2b67ed84ed4afe81da06f5cc288b4d9082da
Eddie Dolan <dolan.charles08@gmail.com>
2013-10-10 21:22:46 -0500

    Initial Commit

---
+++ Euler 40/Euler 40.py
@@ -0,0 +1,22 @@
+__author__ = 'eddolan'
+
+
+def champ(num):
+    sum = 0
+    count = 0
+    while sum < num:
+        count += 1
+        sum += len(str(count))
+    diff = len(str(count)) - (sum - num) - 1
+    return int(str(count)[diff])
+
+## for x in range(1,15):
+##   print x , champ(x)
+
+list = [1,10,100,1000,10000,100000,1000000]
+
+ans = 1
+for count in list:
+    ans = ans * champ(count)
+
+print ans
